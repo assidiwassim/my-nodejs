a090aa32a5ccf58075e90987a29cdb62016a9df3
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-07-19 20:51:27 +0100

    update

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM --platform=arm64 node:18
+
+WORKDIR /my-workspace 
+
+COPY package*.json .
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD [ "npm","start" ]
--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! - test')
 })
+
+
+app.get('/users', (req, res) => {
+    res.send([])
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
