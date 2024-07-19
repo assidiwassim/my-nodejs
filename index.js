192c14d33ce1fc2c6984615ca7ba4aabcd493902
Wassim Assidi <assidi.wassim.dev@gmail.com>
2024-07-19 21:40:05 +0100

    updates

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - test')
+  res.send('Hello World! - hello bro')
 })
 
 
 app.get('/users', (req, res) => {
     res.send([])
+  })
+
+
+  app.get('/test', (req, res) => {
+    res.send('this is a test ...')
   })
 
 app.listen(port, () => {
